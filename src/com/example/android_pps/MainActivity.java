1d55f47f987ba363066c2f74a117f76186f73aa8
Fabrizio Gelsi <fabrizio.gelsi@gmail.com>
2014-06-12 19:45:23 -0300

    Ocultamiento de la barra de título

--- src/com/example/android_pps/MainActivity.java
+++ src/com/example/android_pps/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.android_pps;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//No title bar is set for the activity
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Full screen is set for the Window
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
+		
+		
+		//ActionBar actionBar = getActionBar();
+        //actionBar.hide();
 	}
 
 	@Override
