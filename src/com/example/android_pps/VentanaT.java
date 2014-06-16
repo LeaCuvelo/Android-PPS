ca81a8ba4c142521317f65ca1313c5500de635a1
Fabrizio Gelsi <fabrizio.gelsi@gmail.com>
2014-06-16 18:49:58 -0300

    Enlace de los botones de la ventana T y sus respectivos controladores con su funcionalidad (incompleta)

--- src/com/example/android_pps/VentanaT.java
+++ src/com/example/android_pps/VentanaT.java
@@ -1,15 +1,33 @@
 package com.example.android_pps;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class VentanaT extends Activity {
+	private Button bttnSeT,bttnSeU,bttnSeV,bttnSeX,bttnSeY;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.ventanat);
+		
+		//Mostramos la actividad
+        setContentView(R.layout.activity_main);
+        
+        //Enlazamos botones con sus respectivas acciones
+        bttnSeT = (Button) findViewById(R.id.bttnSeT);
+        bttnSeT.setOnClickListener(controladorT);
+        bttnSeU = (Button) findViewById(R.id.bttnSeU);
+        bttnSeU.setOnClickListener(controladorU);
+        bttnSeV = (Button) findViewById(R.id.bttnSeV);
+        bttnSeV.setOnClickListener(controladorV);
+        bttnSeX = (Button) findViewById(R.id.bttnSeX);
+        bttnSeX.setOnClickListener(controladorX);
+        bttnSeY = (Button) findViewById(R.id.bttnSeY);
+        bttnSeY.setOnClickListener(controladorY);
 	}
 
 	@Override
@@ -18,4 +36,50 @@ public class VentanaT extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	//Controladores de los botones, determinan la acción luego de presionar el botón
+	View.OnClickListener controladorT = new View.OnClickListener() {
+	    public void onClick(View v) {
+	    	//Acción al hacer click   
+	        Intent activityChangeIntent = new Intent(VentanaT.this, MainActivity.class);
+	        // TODO: Escribir la letra T en el textField
+	        VentanaT.this.startActivity(activityChangeIntent);
+	    }
+	};
+	
+	View.OnClickListener controladorU = new View.OnClickListener() {
+	    public void onClick(View v) {
+	    	//Acción al hacer click   
+	        Intent activityChangeIntent = new Intent(VentanaT.this, MainActivity.class);
+	        // TODO: Escribir la letra U en el textField
+	        VentanaT.this.startActivity(activityChangeIntent);
+	    }
+	};
+	
+	View.OnClickListener controladorV = new View.OnClickListener() {
+	    public void onClick(View v) {
+	    	//Acción al hacer click   
+	        Intent activityChangeIntent = new Intent(VentanaT.this, MainActivity.class);
+	        // TODO: Escribir la letra T en el textField
+	        VentanaT.this.startActivity(activityChangeIntent);
+	    }
+	};
+	
+	View.OnClickListener controladorX = new View.OnClickListener() {
+	    public void onClick(View v) {
+	    	//Acción al hacer click   
+	        Intent activityChangeIntent = new Intent(VentanaT.this, MainActivity.class);
+	        // TODO: Escribir la letra T en el textField
+	        VentanaT.this.startActivity(activityChangeIntent);
+	    }
+	};
+	
+	View.OnClickListener controladorY = new View.OnClickListener() {
+	    public void onClick(View v) {
+	    	//Acción al hacer click   
+	        Intent activityChangeIntent = new Intent(VentanaT.this, MainActivity.class);
+	        // TODO: Escribir la letra T en el textField
+	        VentanaT.this.startActivity(activityChangeIntent);
+	    }
+	};
 }
