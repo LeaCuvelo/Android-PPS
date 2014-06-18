8e1d46a265372c741d1d3624e41a6e2a842ba1fa
Fabrizio Gelsi <fabrizio.gelsi@gmail.com>
2014-06-18 18:34:08 -0300

    Actualización ventana

--- src/com/example/android_pps/VentanaA.java
+++ src/com/example/android_pps/VentanaA.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 
 public class VentanaA extends Activity {
